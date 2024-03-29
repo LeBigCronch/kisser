6952a75e819693e4d2eb820ceedc9268bfd45451
LeBigCronch <100243872+LeBigCronch@users.noreply.github.com>
2024-03-29 00:22:33 -0400

    Update kisser.c
    BIG UPGRADES today 
    - got rid of string.h
    - ascii boykisser is now read in the same file 
    performance improvement from ~405 microseconds / execution to 370 in my testing
    
    - input string is now dynamically allocated and not limited to 1024 chars

--- kisser.c
+++ kisser.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+char* joe = NULL;
+size_t size = 0, len = 0;
+
+char g[] = "⠀⢸⠂⠀⠀⠀⠘⣧⠀⠀⣟⠛⠲⢤⡀⠀⠀⣰⠏⠀⠀⠀⠀⠀⢹⡀\n⠀⡿⠀⠀⠀⠀⠀⠈⢷⡀⢻⡀⠀⠀⠙⢦⣰⠏⠀⠀⠀⠀⠀⠀⢸⠀\n⠀⡇⠀⠀⠀⠀⠀⠀⢀⣻⠞⠛⠀⠀⠀⠀⠻⠀⠀⠀⠀⠀⠀⠀⢸⠀\n⠀⡇⠀⠀⠀⠀⠀⠀⠛⠓⠒⠓⠓⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀\n⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀\n⠀⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⠀⠀⢀⡟⠀\n⠀⠘⣇⠀⠘⣿⠋⢹⠛⣿⡇⠀⠀⠀⠀⣿⣿⡇⠀⢳⠉⠀⣠⡾⠁⠀\n⣦⣤⣽⣆⢀⡇⠀⢸⡇⣾⡇⠀⠀⠀⠀⣿⣿⡷⠀⢸⡇⠐⠛⠛⣿⠀\n⠹⣦⠀⠀⠸⡇⠀⠸⣿⡿⠁⢀⡀⠀⠀⠿⠿⠃⠀⢸⠇⠀⢀⡾⠁⠀\n⠀⠈⡿⢠⢶⣡⡄⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⣴⣧⠆⠀⢻⡄⠀⠀\n⠀⢸⠃⠀⠘⠉⠀⠀⠀⠠⣄⡴⠲⠶⠴⠃⠀⠀⠀⠉⡀⠀⠀⢻⡄⠀\n⠀⠘⠒⠒⠻⢦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⠞⠛⠒⠛⠋⠁⠀\n⠀⠀⠀⠀⠀⠀⠸⣟⠓⠒⠂⠀⠀⠀⠀⠀⠈⢷⡀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠙⣦⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⣼⣃⡀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠉⣹⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠀⠀\n";
+
+void render(){
+    if (joe != NULL) {
+        joe[len] = '\0';
+        printf("%s\n", joe);
+    }
+}
 
 int main(){
-	char joe[1024]; 
-	char accum[8096];
-	accum[0] = '\0';
+    int ch;
 
-	while(fgets(joe, 1024, stdin) != NULL){
-		strcat(accum, joe);
-	}
-	printf("OOoooooh you like kissing\n %s don't you, you're a\n %s kisser\n", accum, accum);
-
-	char *home = getenv("HOME");
-	char file_path[1000];
-    snprintf(file_path, sizeof(file_path), "%s/Documents/programming_things/c/actualstuff/kisser/kisser.txt", home);
-
-    FILE *fp = fopen(file_path, "r");
-	if(fp == NULL){
-		perror("");
-		return -1;
-	}
+    while ((ch=getchar()) != EOF) {
+        if (len + 1 >= size){
+            size = size * 2 + 1;
+            joe = realloc(joe, sizeof(char)*size);
+        }
+        joe[len++] = ch;
+    }
 	
-	char chunk[512];
-	while(fgets(chunk, 512, fp) != NULL){
-		printf("%s", chunk);
-	}
-	fclose(fp);
+	printf("OOoooooh you like kissing\n");
+	render();
+	printf("don't you, you're a\n");
+	render();
+	printf("kisser\n");
+	free(joe);
+
+	puts(g);
 }
